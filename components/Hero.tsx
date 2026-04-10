bbe451918e46534d08e9061b5ee89684296b42ba
drich <friedrich0737@gmail.com>
2026-04-10 12:29:14 +0100

    'new'

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,11 +9,11 @@ export function Hero() {
         <span className="text-[var(--color-hero-text)]">sans embaucher 10 personnes</span>
       </h1>
 
-      <p style={{ fontWeight: 400 }} className="lg:w-[859px] h-[84px] dark:w-[819px] text-[24px] leading-[33px] text-[var(--color-hero-text-2)] mx-auto mb-10">
+      <p style={{ fontWeight: 400 }} className="lg:w-[859px] dark:w-[819px] text-[24px] leading-[33px] text-[var(--color-hero-text-2)] mx-auto mb-10">
         Maximisez votre chiffre d'affaires avec des outils dotés d'intelligence artificielle, qui automatisent l'acquisition, les ventes et la gestion de la relation client pour une croissance sans effort.
       </p>
 
-      <div className="flex flex-col sm:flex-row items-center mt-[140px] lg:mt-[95px] justify-center gap-3">
+      <div className="flex flex-col sm:flex-row items-center lg:mt-[95px] justify-center gap-3">
         <button className="px-8 py-3 w-[337px] h-[50px] bg-[var(--color-primary)] text-white font-semibold rounded-[16px] hover:bg-[var(--color-primary-dark)] transition-colors">
           Découvrir nos offres →
         </button>
